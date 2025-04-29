4f25b8275c65458924cdbad2c194332e114df913
yurkevichsergey <sergey.yurkevich.93@gmail.com>
2025-04-29 11:53:12 +0300

    Create task_10.3.py

---
+++ Home_Work_10/task_10.3.py
@@ -0,0 +1,11 @@
+# Ваша функція is_even повинна повертати True якщо число парне, і False якщо число непарне.
+# Вхідні дані: Ціле число.
+# Вихідні дані: Логічний тип.
+def is_even(digit):
+    return digit % 2 == 0
+
+assert is_even(2) == True, 'Test1'
+assert is_even(5) == False, 'Test2'
+assert is_even(0) == True, 'Test3'
+
+print('OK')
