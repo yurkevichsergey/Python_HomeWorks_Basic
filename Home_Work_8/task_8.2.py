3fa35aef1ae2ba1b5605a315127ff0492ee8fb26
yurkevichsergey <sergey.yurkevich.93@gmail.com>
2025-04-29 11:48:11 +0300

    Create task_8.2.py

---
+++ Home_Work_8/task_8.2.py
@@ -0,0 +1,13 @@
+# Ваше завдання – написати функцію is_palindrome, яка перевірятиме, чи є рядок паліндромом.
+# Паліндромом - це такий рядок, який читається однаково зліва направо і зправа наліво без урахування знаків пунктуації та розмірності букв.
+# Функція приймає на вхід рядок, та повертає булеве значення True або False
+def is_palindrome(text):
+    cleaned = ''.join(c.lower() for c in text if c.isalnum())  # лишаємо тільки букви і цифри, переводимо в нижній регістр
+    return cleaned == cleaned[::-1]  # перевірка на паліндром
+
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+
+print("ОК")
