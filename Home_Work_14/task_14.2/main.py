b475fe8cc23437bfee1882afd114cf5623802b79
yurkevichsergey <sergey.yurkevich.93@gmail.com>
2025-04-29 11:59:13 +0300

    Create main.py

---
+++ Home_Work_14/task_14.2/main.py
@@ -0,0 +1,19 @@
+from student import Student
+from group import Group
+
+# Створення студентів
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+
+gr = Group('PD1')
+
+gr.add_student(st1)
+gr.add_student(st2)
+
+print(gr)
+
+assert gr.find_student('Jobs') == st1
+assert gr.find_student('Jobs2') is None
+
+gr.delete_student('Taylor')
+print(gr)
